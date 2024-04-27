97629045606728474d3b7836433edd2b4657b298
guskirb <anguskirby1998@gmail.com>
2024-04-27 23:12:51 +0100

    Add graph import

--- knight.js
+++ knight.js
@@ -1,34 +1,20 @@
-
+import Graph from "./graph";
 
 function knightMoves(start, end) {
-    let queue = [];
-    if (start === end) {
-        return start;
-    }
 
-    if ((start[0] + 1) <= 7 && (start[1] + 2) <= 7) {
-        queue.push(knightMoves([start[0] + 1, start[1] + 2], end));
-    }
-    if ((start[0] + 2) <= 7 && (start[1] + 1) <= 7) {
-        queue.push(knightMoves([start[0] + 2, start[1] + 1], end));
-    }
 }
 
 function getMoves(array) {
-    const allMoves = [];
+    let allMoves = [];
 
     allMoves.push([array[0] + 1, array[1] + 2]);
     allMoves.push([array[0] + 2, array[1] + 1]);
-    allMoves.push([array[0] + 1, array[1] - 2]);
-    allMoves.push([array[0] + 2, array[1] - 1]);
     allMoves.push([array[0] - 1, array[1] - 2]);
     allMoves.push([array[0] - 2, array[1] - 1]);
+    allMoves.push([array[0] + 1, array[1] - 2]);
+    allMoves.push([array[0] + 2, array[1] - 1]);
     allMoves.push([array[0] - 1, array[1] + 2]);
     allMoves.push([array[0] - 2, array[1] + 1]);
 
     return allMoves.filter((item) => item[0] <= 7 && item[1] <= 7 && item[0] >= 0 && item[1] >= 0);
 }
-
-
-console.log(getMoves([4, 1]));
-// knightMoves([0, 0], [7, 7]);
