14d45a0b5bcb0f8b18a86a76942dd77393318abd
guskirb <anguskirby1998@gmail.com>
2024-04-28 06:11:36 +0100

    Add predecessor property

--- node.js
+++ node.js
@@ -1,13 +1,23 @@
 let NewNode;
 
-export default newNode = class Node {
+export default NewNode = class Node {
     constructor(value) {
         this.value = value;
         this.adjacents = [];
+        this.predecessor = null;
     }
 
-    addAdjacent(node) {
-        this.adjacents.push(node);
+    addAdjacent(value) {
+        let duplicate = false;
+        this.adjacents.forEach((adjacent) => {
+            if ((adjacent[0] === value[0]) && (adjacent[1] === value[1])) {
+                duplicate = true;
+            }
+        })
+
+        if (duplicate === false) {
+            this.adjacents.push(value);
+        }
     }
 
     getAdjacent() {
